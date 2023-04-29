98ed02667943c6cb75e7f53822d9a115ecbe4ac9
Otroletravaladna <crnana98@gmail.com>
2023-04-29 02:32:44 -0300

    Remove unnecessary content

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import styles from './page.module.css'
-import Footer from './components/Footer'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Welcome to Memory Card Football</h1>
       <h2>Please select Difficulty</h2>
+      
     </main>
   )
 }
