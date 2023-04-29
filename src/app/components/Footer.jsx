d6cc199e0491e1e73cf72173f16f237a4f7e9142
Otroletravaladna <crnana98@gmail.com>
2023-04-29 01:22:42 -0300

    Add link to github profile

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,7 +5,12 @@ export default function Footer() {
     return(
         <div className={styles.footer}> 
             Project created for Odin Project by Carlos Nana
-            <Image src="github-mark.svg" alt="github" height={22} width={22}/>
+            <a href="https://github.com/capitan-beto" target="_blank">
+                <Image src="github-mark.svg"
+                 alt="github"
+                 height={22} width={22}
+                 />
+            </a>
         </div>
     )
 }
