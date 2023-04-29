3008b4cf996d869fb62d30a69aecbf6d997806ea
Otroletravaladna <crnana98@gmail.com>
2023-04-29 04:41:31 -0300

    Edit layout of teams and style

--- src/app/ChooseLevel.jsx
+++ src/app/ChooseLevel.jsx
@@ -1,45 +1,63 @@
+"use client"
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./page.module.css"
+import { useState } from "react";
+
 
 const links = [{
-    label: "Easy",
+    label: "Fácil",
     route: "/easy",
-    name: "Argentina 1986",
+    name: "Argentina Campeón del Mundo 1986",
     src: "argentina.svg",
     alt: "Argentina"
 }, {
-    label: "Medium",
+    label: "Media",
     route: "/medium",
-    name: "Boca 2003",
+    name: "Boca Campeón Intercontinental 2003",
     src: "boque.svg",
     alt: "Boca Juniors"
 }, {
-    label: "Hard",
+    label: "Difícil",
     route: "/hard",
-    name: "Ferro 1982",
+    name: "Ferro Campeón Invicto 1982",
     src: "fco.svg",
     alt: "Ferrocarril Oeste"
 }];
 
 export default function ChooseLevel() {
+    const [team, setTeam] = useState("Elegí un equipo");
+    const [level, setLevel] = useState(null);
 
+    const handleMouseEnter = (team, level) => {
+        setTeam(team);
+        setLevel(level);
+    }
+
+    const handleMouseLeave = () => {
+        setTeam("Seleccioná un equipo");
+        setLevel(null);
+
+    }
 
     return(
         <div className={styles.chooseLevel}>
             <nav>
+                <h1>{team}</h1>
+                <h2>Dificultad: {level}</h2>
                 <ul>
                     {links.map(({ label, route, name, src, alt }) => {
-                        return <li key={route}>
+                        return <li key={route}
+                          onMouseEnter={() => handleMouseEnter(name, label)}
+                          onMouseLeave={() => handleMouseLeave()}
+                        >
                             <Link href={route}>
-                                {label}
                                 <Image
                                   src={src}
                                   alt={alt}
-                                  width={64}
-                                  height={64}
+                                  width={128}
+                                  height={128}
                                   />
-                                  {name}
                             </Link>
                         </li>
                     })}
