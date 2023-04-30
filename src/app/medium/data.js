eb721d232e13b5aa3a0a57194ef5109a49121d08
Otroletravaladna <crnana98@gmail.com>
2023-04-30 01:00:10 -0300

    Add players data

---
+++ src/app/medium/data.js
@@ -0,0 +1,50 @@
+export const players = [{
+    name: "Abbondanzieri",
+    alt: "Abbondanzieri", 
+    src: ""
+}, {
+    name: "Perea",
+    alt: "Perea",
+    src: ""
+}, {
+    name: "Schiavi",
+    alt: "Schiavi",
+    src: ""
+}, {
+    name: "Burdisso",
+    alt: "Burdisso",
+    src: ""
+}, {
+    name: "Rodríguez",
+    alt: "Rodríguez",
+    src: ""
+}, {
+    name: "Cagna",
+    alt: "Cagna",
+    src: ""
+}, {
+    name: "Cascini",
+    alt: "Cascini",
+    src: ""
+}, {
+    name: "Battaglia",
+    alt: "Battaglia",
+    src: ""
+}, {
+    name: "Donnet",
+    alt: "Donnet",
+    src: ""
+}, {
+    name: "Barros Schelotto",
+    alt: "Barros Schelotto",
+    src: ""
+}, {
+    name: "Larley",
+    alt: "Larley",
+    src: ""
+}, {
+    name: "Bianchi",
+    alt: "Bianchi",
+    src: ""
+}
+]
