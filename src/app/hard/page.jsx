659b6911911b051882e5119b26c90cd861a2f677
Otroletravaladna <crnana98@gmail.com>
2023-05-03 20:26:57 -0300

    Edit page content

--- src/app/hard/page.jsx
+++ src/app/hard/page.jsx
@@ -1,5 +1,27 @@
+import styles from "../easy/page.module.css";
+import Image from "next/image";
+import { players } from "./data";
+
 export default function Hard() {
-    return(
-        <h1>Hola!</h1>
+
+    return (
+        <main className={styles.main}>
+            <nav>
+                <ul className={styles.container}>
+                    {players.map(({ name, alt, src }) => {
+                        return <li key={name} className={styles.imageContainer}>
+                            <Image
+                            className={styles.image}
+                              alt={alt}
+                              src={src}
+                              width={120}
+                              height={150}
+                            />
+                            {name}
+                        </li>
+                    })}
+                </ul>
+            </nav>
+        </main>
     )
 }
