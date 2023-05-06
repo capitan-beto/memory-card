df46f9aa9fdfaa961e16f2a030c141e2969805db
Otroletravaladna <crnana98@gmail.com>
2023-05-06 09:58:38 -0300

    Add shuffle fn when click on img

--- src/app/easy/page.jsx
+++ src/app/easy/page.jsx
@@ -1,17 +1,27 @@
+"use client"
+
 import styles from "./page.module.css";
 import Image from "next/image";
-import { players } from "./data";
+import { players} from "./data";
+import { sort } from "../gameLogic";
+import { useState } from "react";
 
 export default function Easy() {
+    const [items, setItems] = useState(players);
+
+    const handleItems = () => {
+        setItems([...sort(items)]);
+    }
 
     return (
         <main className={styles.main}>
             <nav>
                 <ul className={styles.container}>
-                    {players.map(({ name, alt, src }) => {
+                    {items.map(({ name, alt, src }) => {
                         return <li key={name} className={styles.imageContainer}>
                             <Image
-                            className={styles.image}
+                              onClick={() => handleItems()}
+                              className={styles.image}
                               alt={alt}
                               src={src}
                               width={120}
