270aae3fcd5bd8a43b5a8a78da6e56385ea8767f
Otroletravaladna <crnana98@gmail.com>
2023-05-08 15:23:37 -0300

    Set hard level

--- src/app/hard/page.jsx
+++ src/app/hard/page.jsx
@@ -1,17 +1,52 @@
+"use client"
+
 import styles from "../easy/page.module.css";
 import Image from "next/image";
 import { players } from "./data";
+import { sort, check } from "../gameLogic";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Hard() {
+    const [items, setItems] = useState(players);
+    const [score, setScore] = useState(0);
+    const [visited, setVisited] = useState([]);
+
+    const handleItems = (name) => {
+        if (check(name, visited)) {
+            setScore(0);
+            setVisited([]);
+        } else {
+            setVisited([...visited, name]);
+            setScore(prev => prev + 1);
+        }
+        setItems([...sort(items)]);
+    }
+
+    const Score = () => {
+        if (score  === 12) return <h3>Congrats you won!
+             Try other level or click a player to play again...
+            </h3>;
+        return <h3>Score: {score}</h3>
+    }
+
 
     return (
         <main className={styles.main}>
+            <div>
+                <Link href="/"><h3>Menu</h3></Link>
+                <Score/>
+            </div>
             <nav>
                 <ul className={styles.container}>
-                    {players.map(({ name, alt, src }) => {
-                        return <li key={name} className={styles.imageContainer}>
+                    {items.map(({ name, alt, src }) => {
+                        return <li 
+                          key={name} 
+                          className={styles.imageContainer}
+                          onClick={() => handleItems(name)}
+                        >
                             <Image
-                            className={styles.image}
+                              className={styles.image}
                               alt={alt}
                               src={src}
                               width={120}
