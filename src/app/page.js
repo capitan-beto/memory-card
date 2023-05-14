6286be468f61cfaea12ee8b7337e2824523905a4
Otroletravaladna <crnana98@gmail.com>
2023-05-13 23:00:14 -0300

    Edit home page content and layout

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,8 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Welcome to Memory Card Football</h1>
+      <h1>Bienvenido a Memory Card Fútbol</h1>
       <ChooseLevel/>
-      <h2>Please select Difficulty</h2>
     </main>
   )
 }
